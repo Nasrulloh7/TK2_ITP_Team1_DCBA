c7e30d899203747a2ed1564c6f9bd0683e6ff029
Nasrulloh <ilmi.nasrulloh@gmail.com>
2023-03-23 14:41:38 +0700

    Soal Nomor 1

--- src/deretBilangan.java
+++ src/deretBilangan.java
@@ -8,12 +8,41 @@
  *         Nurul Amalia			-   2602243993
  *         Saur Parlindungan Situmeang	-   2602239056
  */
+
+import java.util.Scanner;
+
 public class deretBilangan {
 
     public static void main(String[] args) {
         
-        //jangan lupa dokumentasi
-        System.out.println("Selamat Datang");
+        Scanner input = new Scanner(System.in);
+        String nama, nim;
+
+        
+        // Nomor 1 //
+        // Meminta input nama dari user
+        do{
+          System.out.print("Masukkan Nama (1-25 karakter): ");
+          nama = input.nextLine();
+            // menampilkan pesan error jika Nama kurang dari 1 atau lebih dari 25 karakter
+            if (nama.length() < 1 || nama.length() > 25){
+                System.out.println("Nama harus memiliki panjang 1-25 karakter. Silakan coba lagi: ");
+            }
+        }while (nama.length() < 1 || nama.length() > 25);
+        // Nomor 1 Selesai //
+        
+        
+        // Nomor 2, Mulai di sini! //
+        
+        // Nomor 2 //
+        
+        
+        System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        
+        System.out.println("Registrasi Sukses..");
+        System.out.printf("Selamat datang %s [NIM: ] \n", nama);
+        
+        System.out.println("\nMari belajar macam-macam deret bilangan..\n");
     }
     
 }
