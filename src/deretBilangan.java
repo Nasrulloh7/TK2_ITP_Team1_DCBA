0adc04c2c7241b9e7fbdd6ad40aad59f31178384
Nasrulloh <ilmi.nasrulloh@gmail.com>
2023-03-27 00:41:23 +0700

    final commit

--- src/deretBilangan.java
+++ src/deretBilangan.java
@@ -14,100 +14,98 @@ import java.util.Scanner;
 public class deretBilangan {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        
         Scanner input = new Scanner(System.in);
         String nama, nim, repetisi;
+        boolean val_repetisi = false;
+        boolean val_stop = false;
         int angka = 0;
-        int hasil_genap = 0;
-        int hasil_ganjil = 0;
-        int bilangan1, bilangan2, hasil_fibonacci,sum_fibonacci;        
+        int hasil_genap, hasil_ganjil;
+        int bilangan1, bilangan2, hasil_fibonacci,sum_fibonacci;
         
+    do{
         // Nomor 1 //
         // Meminta input nama dari user
         do{
-          System.out.print("Masukkan Nama (1-25 karakter): ");
+          System.out.print("\nMasukkan Nama (1-25 karakter): ");
           nama = input.nextLine();
-            // menampilkan pesan error jika Nama kurang dari 1 atau lebih dari 25 karakter
-            if (nama.length() < 1 || nama.length() > 25){
-                System.out.println("Nama harus memiliki panjang 1-25 karakter. Silakan coba lagi: ");
-            }
         }while (nama.length() < 1 || nama.length() > 25);
         // Nomor 1 Selesai //
         
-        
-        // Nomor 2, Mulai di sini! //
+        // Nomor 2 //
+        // Meminta input NIM dari user
         do {
             System.out.print("Masukkan NIM anda [harus 10 karakter]: ");
-            nim = scanner.next();
+            nim = input.nextLine();
         } while(nim.length() != 10 || !nim.matches("[0-9]+"));
-
-        
         // Nomor 2 //
         
-        
+        // Menampilkan hasil registrasi Nama dan NIM
         System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
         
         System.out.println("Registrasi Sukses..");
-        System.out.printf("Selamat datang %s [NIM: %s ] \n", nama,nim);
+        System.out.printf("Selamat datang %s [NIM: %s] \n", nama,nim);
         
         System.out.println("\nMari belajar macam-macam deret bilangan..\n");
         
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         
         // Nomor 3 //
+        // Meminta input jumlah angka dari user
         do {
-            System.out.print("Masukkan Sembarang Angka (5..20) :");
-            angka = scanner.nextInt();
+            System.out.print("Masukkan Sembarang Angka (5..20) : ");
+            angka = input.nextInt();
         } while (angka < 5 || angka > 20);
         
         System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
         System.out.println ("Deret Bilangan");
         System.out.println ("##############\n");
-
         // Nomor 3 selesai //
         
 
-         // Nomor 4 //
+        // Nomor 4 //
+        // Menampilkan deret bilangan genap
+        System.out.println (angka + " Bilangan Genap : ");
+        hasil_genap = 0;
+        for (int a=2; a<=angka*2; a++){
+            if (a%2==0) {
+                System.out.print(a+" ");
+                hasil_genap += a;
+            } 
+        }
+        System.out.printf("\nHasil Pemjumlahan : %s \n", hasil_genap);
  
-         System.out.println (angka + " Bilangan Genap : ");
-         for (int a=2; a<=angka*2; a++){
-             if (a%2==0) {
-                 System.out.print(a+" ");
-                 hasil_genap = hasil_genap + a;
-             } 
-         }
-         System.out.print("\nHasil ialah :  " + hasil_genap);
+        // Menampilkan deret bilangan ganjil
+        System.out.println ("\n" + angka + " Bilangan Ganjil : ");
+        hasil_ganjil = 0;
+        for (int a=1; a<=angka*2; a++){
+            if (a%2!=0) {
+                System.out.print(a+" ");
+                hasil_ganjil += a;
+            } 
+        }
+        System.out.printf("\nHasil Pemjumlahan : %s \n", hasil_ganjil);
  
- 
-         System.out.println ("\n" + angka + " Bilangan Ganjil : ");
-         for (int a=1; a<=angka*2; a++){
-             if (a%2!=0) {
-                 System.out.print(a+" ");
-                 hasil_ganjil = hasil_ganjil + a;
-             } 
-         }
-         System.out.print("\nHasil ialah :  " + hasil_ganjil);
- 
-         System.out.println ("\n" + angka + " Bilangan Fibonacci : ");
-         bilangan1 = 1;
-         bilangan2 = 1;
-         hasil_fibonacci = 0;
-         sum_fibonacci = 0;
-         for (int a=1; a<=angka; a++){
-             System.out.print(bilangan1+", ");
-             hasil_fibonacci = bilangan1 + bilangan2;
-             bilangan2 = bilangan1;
-             bilangan1 = hasil_fibonacci;
-             sum_fibonacci = sum_fibonacci + hasil_fibonacci;             
-         }
-         System.out.print("\nHasil ialah :  " + sum_fibonacci);         
-         
-         // Nomor 4 Selesai //
+        // Menampilkan deret bilangan fibonacci
+        System.out.println ("\n" + angka + " Bilangan Fibonacci : ");
+        bilangan1 = 1;
+        bilangan2 = 1;
+        sum_fibonacci = 0;
+        for (int a=0; a<angka; a++){
+            hasil_fibonacci = bilangan1;
+            System.out.print(hasil_fibonacci + " ");
+            sum_fibonacci += hasil_fibonacci;
+            bilangan1 = bilangan2;
+            bilangan2 = hasil_fibonacci + bilangan1;             
+        }
+        System.out.printf("\nHasil Pemjumlahan : %s \n", sum_fibonacci );         
+        // Nomor 4 Selesai //
 
 
-        //====Nomor 5 Mulai dari sini//
+        // Nomor 5 //
+        // Memberi pilihan kepada user untuk mengulang program atau tidak
         do {
-        System.out.print("Anda ingin mengulang [y/t]: ");
+        System.out.print("\nAnda ingin mengulang [y/t]: ");
         repetisi = input.next();
             if("t".equals(repetisi) || "T".equals(repetisi)
                 || "y".equals(repetisi) || "Y".equals(repetisi))
@@ -122,13 +120,10 @@ public class deretBilangan {
                                 {
                                 System.out.println("\nKarakter Nama Tidak Sesuai Ketentuan"); 
                                 }
-                    }
-        while(!val_repetisi);
-            System.out.println("\n\n\n\n");
-                    }
-        while(!val_stop);
-    //====Nomor 5 Selesai//
-
+            }while(!val_repetisi);
+            System.out.println("\n");
+        }while(!val_stop);
+        // Nomor 5 Selesai //
         
     }
     
